627f5e53dc0d8f47b41391b909d1718cdc008a84
Dan Martens <dan@friendsoftheweb.com>
2017-05-16 16:01:08 -0400

    Handle non-TTY environments in spinnerSpawn

--- utils/spinner-spawn.js
+++ utils/spinner-spawn.js
@@ -7,40 +7,52 @@ function spinnerSpawn(command, args, message) {
   args = compact(args);
 
   return new Promise((resolve, reject) => {
-    const spinner = Spinner();
-
     process.stderr.write(chalk.dim(`\n$ ${command} ${args.join(' ')}\n`));
-
-    let lastSpinner = '';
-
-    const spinnerInterval = setInterval(() => {
-      lastSpinner = '\r\x1B[36m' + message + '\x1B[m ' + spinner.next();
-      process.stdout.write(lastSpinner);
-    }, 100);
 
     const cmd = spawn(command, args);
 
-    cmd.stdout.on('data', data => {
-      process.stdout.clearLine();
-      process.stdout.cursorTo(0);
+    if (process.stdout.isTTY) {
+      const spinner = Spinner();
 
-      process.stdout.write(data);
-      process.stdout.write(lastSpinner);
-    });
+      let lastSpinner = '';
 
-    cmd.stderr.on('data', data => {
-      process.stderr.clearLine();
-      process.stderr.cursorTo(0);
+      const spinnerInterval = setInterval(() => {
+        lastSpinner = '\r\x1B[36m' + message + '\x1B[m ' + spinner.next();
+        process.stdout.write(lastSpinner);
+      }, 100);
 
-      process.stderr.write(data);
-    });
+      cmd.stdout.on('data', data => {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+
+        process.stdout.write(data);
+        process.stdout.write(lastSpinner);
+      });
+
+      cmd.stderr.on('data', data => {
+        process.stderr.clearLine();
+        process.stderr.cursorTo(0);
+
+        process.stderr.write(data);
+      });
+
+      cmd.on('close', code => {
+        clearInterval(spinnerInterval);
+
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+      });
+    } else {
+      cmd.stdout.on('data', data => {
+        process.stdout.write(data);
+      });
+
+      cmd.stderr.on('data', data => {
+        process.stderr.write(data);
+      });
+    }
 
     cmd.on('close', code => {
-      clearInterval(spinnerInterval);
-
-      process.stdout.clearLine();
-      process.stdout.cursorTo(0);
-
       if (code === 0) {
         resolve(code);
       } else {
