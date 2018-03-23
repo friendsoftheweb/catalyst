2943bd2797f2ce516764428b68b54501dab560a9
Dan Martens <dan@friendsoftheweb.com>
2018-03-23 12:47:53 -0400

    Fix tests

--- test/config-test.js
+++ test/config-test.js
@@ -1,10 +1,10 @@
 const expect = require('expect');
 const path = require('path');
 
-const generateEntry = require('../config/webpack/generate-entry');
-const generateOutput = require('../config/webpack/generate-output');
-const generatePlugins = require('../config/webpack/generate-plugins');
-const generateRules = require('../config/webpack/generate-rules');
+const generateEntry = require('../config/webpack/generateEntry');
+const generateOutput = require('../config/webpack/generateOutput');
+const generatePlugins = require('../config/webpack/generatePlugins');
+const generateRules = require('../config/webpack/generateRules');
 
 const testProjectRoot = path.resolve(__dirname, './project');
 
