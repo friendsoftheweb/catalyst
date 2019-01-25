946ca6ebedbc15c41df42ff2f4231ee72cc1cbcf
Dan Martens <dan@friendsoftheweb.com>
2019-01-25 11:47:52 -0500

    Turn Flow/TypeScript support on based on config

--- src/utils/getEnvironment.js
+++ src/utils/getEnvironment.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const getDirectories = require('./getDirectories');
+
 function getEnvironment() {
+  const directories = getDirectories();
   const env = process.env.NODE_ENV;
 
   const devServerHost = process.env.DEV_SERVER_HOST || 'localhost';
   const devServerPort = process.env.DEV_SERVER_PORT || 8080;
 
+  const typeScriptConfigExists = fs.existsSync(
+    path.join(directories.project, 'tsconfig.json')
+  );
+
+  const flowConfigExists = fs.existsSync(
+    path.join(directories.project, '.flowconfig')
+  );
+
   return {
     production: env === 'production',
     test: env === 'test',
     development: env !== 'production' && env !== 'test',
+    typeScriptConfigExists,
+    flowConfigExists,
     devServerHost,
     devServerPort
   };
