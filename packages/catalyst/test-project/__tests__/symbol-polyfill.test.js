902ceb2844e8c60c345cb9de214e5a7d349d784a
Dan Martens <martens.dan@gmail.com>
2019-06-29 16:53:05 -0400

    Move tests into jest

---
+++ packages/catalyst/test-project/__tests__/symbol-polyfill.test.js
@@ -0,0 +1,14 @@
+const assertFileContains = require('../assertFileContains');
+
+test('build process polyfills symbols', async () => {
+  await assertFileContains({
+    file: 'symbol-polyfill.js',
+    content:
+      '__webpack_require__(/*! core-js/modules/es.symbol */ "../node_modules/core-js/modules/es.symbol.js");'
+  });
+
+  await assertFileContains({
+    file: 'symbol-polyfill.js',
+    content: "var testSymbol = Symbol('testSymbol');"
+  });
+});
---
+++ packages/catalyst/test-setup.js
@@ -0,0 +1,28 @@
+const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+module.exports = async function() {
+  console.log('\n\nBuilding catalyst...\n');
+
+  execSync('yarn build', { stdio: 'inherit' });
+
+  console.log('\nInstalling catalyst into test project...\n');
+
+  execSync('yarn install', {
+    stdio: 'inherit',
+    cwd: join(process.cwd(), 'test-project')
+  });
+
+  console.log('\nRemoving previous test output...\n');
+
+  execSync('rm -rf dist', {
+    stdio: 'inherit',
+    cwd: join(process.cwd(), 'test-project')
+  });
+
+  execSync('NODE_ENV=test yarn run catalyst build', {
+    stdio: 'inherit',
+    cwd: join(process.cwd(), 'test-project')
+  });
+};
