43557b78d1f695d949874dbcb7196894724f3c6c
Dan Martens <martens.dan@gmail.com>
2019-06-29 20:19:06 -0400

    Fix dynamic import test

--- packages/catalyst/test-project/__tests__/dynamic-import.test.ts
+++ packages/catalyst/test-project/__tests__/dynamic-import.test.ts
@@ -1,7 +1,7 @@
 const assertFileContains = require('../assertFileContains');
 
 const content = `
-Promise.all(/*! import() */[__webpack_require__.e("common"), __webpack_require__.e(0)]).then(__webpack_require__.t.bind(null, /*! ./other */ "./bundles/dynamic-import/other.ts", 7)).then(function (_ref) {
+Promise.all(/*! import() */[__webpack_require__.e("common"), __webpack_require__.e(0)]).then(__webpack_require__.t.bind(null, /*! ./HelloWorld */ "./bundles/dynamic-import/HelloWorld.ts", 7)).then(function (_ref) {
   var HelloWorld = _ref["default"];
   new HelloWorld();
 });
