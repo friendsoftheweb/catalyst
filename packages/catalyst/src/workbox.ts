c62274d292d1493471ccdad5653abc5a21dd13c8
Dan Martens <dan@friendsoftheweb.com>
2019-08-12 18:20:23 -0400

    Remove arrow functions from workbox script

--- packages/catalyst/src/workbox.ts
+++ packages/catalyst/src/workbox.ts
@@ -1,13 +1,13 @@
 const SERVICE_WORKER_URL = process.env.SERVICE_WORKER_URL;
 
 if (SERVICE_WORKER_URL != null && 'serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+  window.addEventListener('load', function() {
     navigator.serviceWorker
       .register(SERVICE_WORKER_URL)
-      .then((registration) => {
+      .then(function(registration) {
         console.log('SW registered: ', registration);
       })
-      .catch((registrationError) => {
+      .catch(function(registrationError) {
         console.log('SW registration failed: ', registrationError);
       });
   });
