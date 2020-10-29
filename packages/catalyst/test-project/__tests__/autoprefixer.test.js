e26ca7a2d591e14f9e71dbe7a8cf965a6e3871ca
Dan Martens <dan@friendsoftheweb.com>
2020-10-29 11:43:00 -0400

    Update catalyst dependencies

--- packages/catalyst/test-project/__tests__/autoprefixer.test.js
+++ packages/catalyst/test-project/__tests__/autoprefixer.test.js
@@ -2,8 +2,6 @@ const assertFileContains = require('../assertFileContains');
 
 const content = `::-webkit-input-placeholder {
   color: gray; }
-::-moz-placeholder {
-  color: gray; }
 :-ms-input-placeholder {
   color: gray; }
 ::-ms-input-placeholder {
