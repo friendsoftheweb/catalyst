2caf91e068103af8a6d41ad8c27da480ccc13913
Dan Martens <dan@friendsoftheweb.com>
2020-10-30 20:15:41 -0400

    Remove usage of lodash.reduce

--- packages/catalyst/src/config/webpack/index.ts
+++ packages/catalyst/src/config/webpack/index.ts
@@ -1,4 +1,3 @@
-import { reduce } from 'lodash';
 import { Configuration as WebpackConfiguration } from 'webpack';
 import generateDevtool from './generateDevtool';
 import generateEntryForBundleName from './generateEntryForBundleName';
@@ -20,23 +19,21 @@ export default function webpackConfig(options?: Options): WebpackConfiguration {
     context: contextPath,
     mode: environment === 'production' ? 'production' : 'development',
     devtool: generateDevtool(),
-    entry: reduce(bundlePaths(), (entry, bundlePath) => {
+    entry: bundlePaths().reduce((entry, bundlePath) => {
       const parts = bundlePath.split('/');
       const bundleName = parts[parts.length - 1];
 
-      return Object.assign({}, entry, {
-        [bundleName]: generateEntryForBundleName(bundleName)
-      });
-    }),
+      return { ...entry, [bundleName]: generateEntryForBundleName(bundleName) };
+    }, {}),
     output: generateOutput(),
     resolve: {
       extensions: ['.wasm', '.mjs', '.js', '.ts', '.tsx', '.json'],
-      modules: [contextPath, 'node_modules']
+      modules: [contextPath, 'node_modules'],
     },
     plugins: generatePlugins(options),
     module: {
-      rules: generateRules()
+      rules: generateRules(),
     },
-    optimization: generateOptimization()
+    optimization: generateOptimization(),
   };
 }
