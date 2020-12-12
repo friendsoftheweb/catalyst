a214cc1e5d513d7328fea712df7c5b01e7dc76c0
Dan Martens <dan@friendsoftheweb.com>
2020-12-12 18:21:18 -0500

    Switch from “node-sass” to “sass” (#61)
    * Switch from “node-sass” to “sass”
    
    * Add “fibers” for “sass”
    
    * Fix test

--- packages/catalyst/test-project/__tests__/autoprefixer.test.js
+++ packages/catalyst/test-project/__tests__/autoprefixer.test.js
@@ -3,8 +3,8 @@ const assertFileContains = require('../assertFileContains');
 const content = `.example {
   -webkit-user-select: none;
       -ms-user-select: none;
-          user-select: none; }
-`;
+          user-select: none;
+}`;
 
 test('build process adds vendor prefixes to CSS', async () => {
   await assertFileContains({
