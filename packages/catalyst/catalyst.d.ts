c42fec0a321790017f0d517e16affb41752f31a6
Dan Martens <dan@friendsoftheweb.com>
2020-12-20 13:42:04 -0500

    Add types for image assets

---
+++ packages/catalyst/catalyst.d.ts
@@ -0,0 +1,41 @@
+declare module '*.gif' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+  export const width: number;
+  export const height: number;
+}
